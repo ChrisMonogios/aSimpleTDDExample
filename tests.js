35398f84a4f9a9a0b1f2d59e7d2e10c29fe67ba3
Chris Monogios <chr_mon_@hotmail.com>
2015-12-27 11:44:20 +0100

    added second test with check on AM. Test fails

--- tests.js
+++ tests.js
@@ -1,3 +1,7 @@
 ﻿QUnit.test( "convert_07:05:45PM", function(assert) {
   assert.equal(convertToMilitaryTime("07:05:45PM"), "19:05:45");
 });
+
+QUnit.test( "convert_07:05:45AM", function(assert) {
+  assert.equal(convertToMilitaryTime("07:05:45AM"), "07:05:45");
+});
