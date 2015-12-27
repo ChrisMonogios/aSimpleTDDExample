27c6faa6fe6d4257eaeb7710f7373496dfb46c70
Chris Monogios <chr_mon_@hotmail.com>
2015-12-27 11:42:37 +0100

    code to pass first test

--- TimeConversion.js
+++ TimeConversion.js
@@ -1,2 +1,9 @@
 ﻿function convertToMilitaryTime(time) {
+        var splittedTime = time.split(":");
+        
+        splittedTime[2] = splittedTime[2].replace(/\D/g, "")
+        splittedTime[0] = parseInt(splittedTime[0]) + 12;
+        
+        return splittedTime.join(":");
+
 }
