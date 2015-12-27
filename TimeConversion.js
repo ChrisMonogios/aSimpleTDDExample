acecc019f69ab283f76cc3851f3e3dd858608355
Chris Monogios <chr_mon_@hotmail.com>
2015-12-27 12:28:55 +0100

    refactored code and added more border-case tests

--- TimeConversion.js
+++ TimeConversion.js
@@ -1,17 +1,42 @@
-﻿function convertToMilitaryTime(time) {
-        var splittedTime = time.split(":");
-        
-        if (splittedTime[2].indexOf("AM") === -1) {
-            if (splittedTime[0] !== "12") {
-                splittedTime[0] = parseInt(splittedTime[0]) + 12; 
-            }
-        } else {
-            if (splittedTime[0] === "12") {
-                splittedTime[0] = "00";
-            }
-        }
-        
-        splittedTime[2] = splittedTime[2].replace(/\D/g, "");
-        
-        return splittedTime.join(":");
+﻿var constants = {
+    AM: "AM",
+    COLON: ":",
+    EMPTY: "",
+    TWELVE_HOURS: "12",
+    TWELVE_HOURS_AS_INT: 12,
+    ZERO_HOURS: "00",
+    HOURS_INDEX: 0,
+    SECONDS_INDEX: 2,
 }
+
+function convertToMilitaryTime(time) {
+        var splittedTime = time.split(constants.COLON);
+        
+        convertToMilitaryTimeWhenAMOrPM(splittedTime);
+        splittedTime[constants.SECONDS_INDEX] = splittedTime[constants.SECONDS_INDEX].replace(/\D/g, constants.EMPTY);
+
+        return splittedTime.join(constants.COLON);
+}
+
+function convertToMilitaryTimeWhenAMOrPM(splittedTime) {
+    if (splittedTime[constants.SECONDS_INDEX].indexOf(constants.AM) > -1) {
+        convertToMilitaryTimeWhenAM(splittedTime);
+    } else {
+        convertToMilitaryTimeWhenPM(splittedTime);
+    }
+}
+
+function convertToMilitaryTimeWhenAM(splittedTime) {
+    if (splittedTime[constants.HOURS_INDEX] === constants.TWELVE_HOURS) {
+        splittedTime[constants.HOURS_INDEX] = constants.ZERO_HOURS;
+    }
+}
+
+function convertToMilitaryTimeWhenPM(splittedTime) {
+    var hours = splittedTime[constants.HOURS_INDEX];
+    
+    if (hours !== constants.TWELVE_HOURS) {
+        splittedTime[constants.HOURS_INDEX] = parseInt(hours) + constants.TWELVE_HOURS_AS_INT; 
+    }
+}
+
--- tests.js
+++ tests.js
@@ -13,3 +13,15 @@ QUnit.test( "convert_12:45:54PM", function(assert) {
 QUnit.test( "convert_12:40:22AM", function(assert) {
   assert.equal(convertToMilitaryTime("12:40:22AM"), "00:40:22");
 });
+
+QUnit.test( "convert_11:59:59PM", function(assert) {
+  assert.equal(convertToMilitaryTime("11:59:59PM"), "23:59:59");
+});
+
+QUnit.test( "convert_12:00:00AM", function(assert) {
+  assert.equal(convertToMilitaryTime("12:00:00AM"), "00:00:00");
+});
+
+QUnit.test( "convert_04:59:59PM", function(assert) {
+  assert.equal(convertToMilitaryTime("04:59:59PM"), "16:59:59");
+});
