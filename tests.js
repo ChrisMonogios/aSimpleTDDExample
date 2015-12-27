2c1a31f99a5e4fed34b5b3bd52d420604fd4e1ac
Chris Monogios <chr_mon_@hotmail.com>
2015-12-27 11:48:25 +0100

    third test with the 'problematic' 12 o'clock. Test fails

--- tests.js
+++ tests.js
@@ -5,3 +5,7 @@
 QUnit.test( "convert_07:05:45AM", function(assert) {
   assert.equal(convertToMilitaryTime("07:05:45AM"), "07:05:45");
 });
+
+QUnit.test( "convert_12:45:54PM", function(assert) {
+  assert.equal(convertToMilitaryTime("12:45:54PM"), "12:45:54");
+});
