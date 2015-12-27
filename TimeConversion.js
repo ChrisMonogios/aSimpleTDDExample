24c1a12b41cca867f7fbd4e54959b67a5a5537e4
Chris Monogios <chr_mon_@hotmail.com>
2015-12-27 11:51:11 +0100

    fixing the border-value of 12 o'clock PM

--- TimeConversion.js
+++ TimeConversion.js
@@ -2,7 +2,9 @@
         var splittedTime = time.split(":");
         
         if (splittedTime[2].indexOf("AM") === -1) {
-            splittedTime[0] = parseInt(splittedTime[0]) + 12;        
+            if (splittedTime[0] !== "12") {
+                splittedTime[0] = parseInt(splittedTime[0]) + 12; 
+            }
         }
         
         splittedTime[2] = splittedTime[2].replace(/\D/g, "");
